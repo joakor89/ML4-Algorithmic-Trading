177036c6ea7d1eedf37cbaade81e3a39ecbd4492
joakor89 <joacorf33@gmail.com>
2025-03-30 12:48:33 -0600

    Scraping procedural files

---
+++ chap_03/02_earning_calls/check_data.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.expand_frame_repr', False)
+np.random.seed(42)
+
+path = Path('transcripts', 'parsed')
+
+files = path.glob(('**/content.csv'))
+words = 0
+for file in files:
+    words += pd.read_csv(file).content.str.split().str.len().sum()
+print(words)
+
+# print(len(list(files)))
